e0d73f161638fc89461f22fb0c0365c587f7a0cf
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-04-04 10:43:00 +0800

    add: custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
       jost: "Jost, sans-serif",
       poppins: "Poppins, sans-serif",
     },
-    extend: {},
+    extend: {
+      colors: {
+        "orange-lemon": "hsl(14, 97%, 60%)",
+        "blue-docker": "hsl(217, 89%, 61%)",
+        "navy-blue": "hsl(218, 39%, 35%)",
+        "youth-blue": "hsl(199, 74%, 59%)",
+        "cream-vanilla": "hsl(52, 60%, 95%)",
+        "yellow-sunrise": "hsl(56, 98%, 59%)",
+      },
+    },
   },
   plugins: [],
 };
